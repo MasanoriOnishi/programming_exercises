eefed9d8a17314d4c13ec3f8b6ef162c5ed476cc
MasanoriOhnishi <onishi@tyrus.co.jp>
2017-11-12 20:21:39 +0900

    httpのリクエストの部分について、現状、このコードではないかと思うところをコミット

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,55 +1,33 @@
 import {run} from '@cycle/rxjs-run'
 import {VNode, makeDOMDriver, div, h2, button, pre, p, code} from '@cycle/dom'
-// import {makeDOMDriver} from '@cycle/dom'
 import Rx from 'rxjs/Rx'
 import xs from 'xstream';
 import {makeHTTPDriver} from '@cycle/http'
-// import {DOMSource} from '@cycle/dom/rxjs-typings'
-// import {HTTPSource} from '@cycle/http/rxjs-typings'
 
-// DOM ツリーを作成
-// function render(PageState) {
-//   return div('.container-fluid', [
-//     // render view ...
-//   ]);
-// }
-
-export function main (sources) {
-  // const defaultPageState = {
-  //   response: {}
-  // };
-  const request$ = xs.periodic(1000)
-    .mapTo({
-      url: 'https://jsonplaceholder.typicode.com/posts',
-      category: 'api',
-    });
-
-  const vtree$ = sources.HTTP.select('api').take(2).map(res => div(res))
-
-  // let request$ = xs.of({
-  //   url: 'http://localhost:3000/todos', // GET method by default
-  //   category: 'api'
-  // });
-  //
-  // let response$ = sources.HTTP.select('api').switchMap(x => x)
-
-  // const vtree$ = Rx.Observable.of(
-  //   '<div>My Awesome Cycle.js app</div>' + response$
-  // )
-
-  // 4. レスポンス Observable から値を取り出して DOM 構造を更新する
-  // const pageState$ = response$.map(response => ({response})).startWith(defaultPageState);
-  // const dom$ = pageState$.map(pageState => render(pageState$));
-
+function main({ DOM, HTTP}) {
+  let request$ = xs.of({
+    url: 'http://127.0.0.1:3000/todos',
+    category: 'api',
+  })
+  const response$ = HTTP.select('api');
+  const vtree$ = xs.of(
+    div(
+      '.container',
+      [
+        h2({response$})
+      ]
+    )
+  )
   const sinks = {
     DOM: vtree$,
     HTTP: request$
   }
   return sinks
 }
+
 const drivers = {
-  DOM: makeDOMDriver('#root'),
+  DOM: makeDOMDriver('body'),
   HTTP: makeHTTPDriver()
 }
 
-run(main, drivers)
+run(main, drivers);
