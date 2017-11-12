8ba2df374865051d21be1f7807c8b52730a5aad3
MasanoriOhnishi <onishi@tyrus.co.jp>
2017-11-12 14:00:32 +0900

    cycle.jsをruby側のapp.jsにコンパイルするところまでつくった

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,18 +1,16 @@
-//* @jsx hJSX
-import Rx from 'rxjs/Rx'
 import {run} from '@cycle/rxjs-run'
 import {makeDOMDriver, hJSX} from '@cycle/dom'
+import Rx from 'rxjs/Rx'
 
-function main (sources) {
+export function main (sources) {
   const vtree$ = Rx.Observable.of(
-    <div>My Awesome Cycle.js app</div>
+    '<div>My Awesome Cycle.js app</div>'
   )
   const sinks = {
     DOM: vtree$
   }
   return sinks
 }
-
 const drivers = {
   DOM: makeDOMDriver('#root')
 }
