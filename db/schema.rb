e50beab70b63b9880991540f58a686148c9c6107
MasanoriOnishi <monarisamasa@gmail.com>
2017-12-04 00:11:30 +0900

    todoモデルにuser_idのカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171112052123) do
+ActiveRecord::Schema.define(version: 20171203145514) do
 
   create_table "todos", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.date "due"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20171112052123) do
     t.string "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "user_id"
   end
 
 end
