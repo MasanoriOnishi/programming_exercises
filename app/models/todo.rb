c76c6bbf5a2bcd0aecc536bf624dc78a8da9cd89
MasanoriOnishi <monarisamasa@gmail.com>
2017-12-24 18:54:52 +0900

    タスクが空白のときにエラーを吐くようにした。

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,7 +1,11 @@
 class Todo < ApplicationRecord
-  validate :due_is_valid_datetime
+  validate :due_is_valid_datetime, :task_is_presence
 
   def due_is_valid_datetime
     errors.add(:due, 'due must be a valid datetime') if ((DateTime.parse(due.to_s) rescue ArgumentError) == ArgumentError)
+  end
+
+  def task_is_presence
+    errors.add(:task, 'task can\'t be blank') if task.blank?
   end
 end
